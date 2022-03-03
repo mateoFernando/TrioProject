8aba92a0a140859bb2a9271776abe9bc54f5feac
Fernando Mateo <ferdamateo@hotmail.com>
2022-03-02 21:05:40 -0500

    testing

---
+++ TrioRestaurant/TrioRestaurant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrioRestaurant
+//
+//  Created by Fernando Daniel on 2/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
